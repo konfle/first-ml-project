c5e453a3a978a46721708702ad0915b2bc7e836e
konfle <konrad.flejter@gmail.com>
2024-02-04 08:38:15 +0100

    Convert str to int in garden column.

--- main.py
+++ main.py
@@ -18,3 +18,12 @@ print(data_encoded.garden.unique())
 print(f"\nThe 5th garden area is: {data_encoded.garden[4]}")
 area = int(re.findall(pattern='\d+', string=data_encoded.garden[4])[0])
 print(f"Just area: {area}")
+
+# Convert strings data type to integer
+for i in range(len(data_encoded)):
+    if data_encoded.loc[i, "garden"] == "Not present":
+        data_encoded.loc[i, "garden"] = 0
+    else:
+        data_encoded.loc[i, "garden"] = int(re.findall(r'\d+', data_encoded.loc[i, "garden"])[0])
+
+print(f"Garden areas: {data_encoded.garden.unique()}")
